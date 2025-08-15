cd2a2f210429d7e9d1856d5475f3d8faade9e9f7
Kisna Goyal <131574334+kisnagoyal@users.noreply.github.com>
2025-08-16 01:00:13 +0530

    Update sendVerificationEmail.ts

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,34 +1,3 @@
-// //done
-
-// import {resend} from "@/lib/resend";
-// import VerificationEmail from "../../emails/VerificationEmail";
-// import {ApiResponse} from "@/types/ApiResponse";
-
-
-
-// export async function sendVerificationEmail(email: string, username: string, verifyCode: string): Promise<ApiResponse> {
-//     try {
-//         console.log(email)
-//         await resend.emails.send({
-//             from: 'onboarding@resend.dev', //prblm can be in this line
-//             to: [email], 
-//             subject: 'PhantomFeedback | Verify your email',
-//             react: VerificationEmail({username, otp: verifyCode})
-//         });
-//         console.log("success", email)
-//         return {
-
-//             success: true,
-//             message: 'Verification email sent'
-//         };
-//     } catch (emailError) {
-//         console.error('Error sending verification email')
-//         return {
-//             success: false,
-//             message: 'message not sent'
-//         };
-//     }
-// }
 
 // src/helpers/sendVerificationEmail.ts
 
